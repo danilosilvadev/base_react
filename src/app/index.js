aeceb0844a259a10a58e1b791280fe2f1c2d8486
Danilo Silva <markpetrelli@gmail.com>
2018-01-26 16:26:08 -0200

    Update index.js

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MyComponent from './components/myComponent';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(<BrowserRouter><MyComponent /></BrowserRouter>, document.getElementById('root'));
